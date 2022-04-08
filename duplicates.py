cec8117e6d3b54b5ee85a26fe0e67b93a24b27ff
Marriane791 <annemarrieakeyo42@gmail.com>
2022-04-08 22:32:00 +0300

    initial commit

---
+++ duplicates.py
@@ -0,0 +1,9 @@
+class Node ():
+    def __init__(self,data):
+        self.data = data
+        self.next =None
+
+class LinkedList():
+    def __init__(self):
+        #linkedlist head
+        self.head = None
